f8a8df1978b35ad58ff779754eca9e81fa67d7d3
Roman Chernyi <79677742428@yandex.ru>
2023-06-29 08:06:33 +0400

    pep8

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -2,7 +2,9 @@
 from http import HTTPStatus
 
 from api import models
+
 from django.test import Client, TestCase
+
 
 class TaskiAPITestCase(TestCase):
     def setUp(self):
@@ -18,4 +20,4 @@ class TaskiAPITestCase(TestCase):
         data = {'title': 'Test', 'description': 'Test'}
         response = self.guest_client.post('/api/tasks/', data=data)
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
-        self.assertTrue(models.Task.objects.filter(title='Test').exists()) 
+        self.assertTrue(models.Task.objects.filter(title='Test').exists())
